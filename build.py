510401cbab9ed0e18a3ad6e5220cd0ae84754455
Oli <OliverWDGarrett@gmail.com>
2023-05-31 22:12:06 +1200

    woops workin now"

--- build.py
+++ build.py
@@ -1,11 +1,18 @@
 
 import os
 
-os.makedirs("build/hello")
+
+DIR = "build/"
+
+os.makedirs(DIR + "hello", exist_ok=True)
 
 
 for i in range(5):
-    with open(f"{i}.txt") as f:
-        f.write(f"hello {i}", "w+")
+    with open(DIR + f"{i}.txt", "w+") as f:
+        f.write(f"hello {i}")
+
+
+with open(DIR + "hello/nested.txt", "w+") as f:
+    f.write("ok, nested works.")
 
 
